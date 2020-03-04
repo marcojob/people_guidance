bda864859381b29587d7c41b2d5b18288ec20594
marcojob <marco.job@bluewin.ch>
2020-03-04 23:40:15 +0100

    minimal imu setup working on imu

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from people_guidance.pipeline import Pipeline
 from people_guidance.utils import init_logging
 
+from people_guidance.modules.drivers_module import DriversModule
+
 if __name__ == '__main__':
     init_logging()
 
     pipeline = Pipeline()
+
+    # Handles hardware drivers and interfaces
+    pipeline.add_module(DriversModule)
+
     pipeline.start()
---
+++ people_guidance/modules/drivers_module/__init__.py
@@ -0,0 +1,1 @@
+from .drivers_module import *
