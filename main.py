b5f37f6fe2cdc684a32efa9cea7814917b2977c2
marcojob <marco.job@bluewin.ch>
2020-03-04 19:57:09 +0100

    cleanup example module

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from people_guidance.modules.spam_module import SpamModule
-from people_guidance.modules.echo_module import EchoModule
 from people_guidance.pipeline import Pipeline
 from people_guidance.utils import init_logging
 
@@ -7,6 +5,4 @@ if __name__ == '__main__':
     init_logging()
 
     pipeline = Pipeline()
-    pipeline.add_module(SpamModule)
-    pipeline.add_module(EchoModule)
     pipeline.start()
