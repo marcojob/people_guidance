55a7a3083d7ad5b863e670b45abe33ff91ddbd23
lorenz-h <lorenz.hetzel@me.com>
2020-03-25 09:34:25 +0100

    small bugfixes in module.py, add visualization module

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from people_guidance.utils import init_logging
 
 from people_guidance.modules.drivers_module import DriversModule
 from people_guidance.modules.fps_logger_module import FPSLoggerModule
+from people_guidance.modules.visualization_module import VisualizationModule
 
 if __name__ == '__main__':
     init_logging()
@@ -29,5 +30,5 @@ if __name__ == '__main__':
     # Handles hardware drivers and interfaces
     pipeline.add_module(DriversModule)
     pipeline.add_module(FPSLoggerModule)
-
+    pipeline.add_module(VisualizationModule)
     pipeline.start()
