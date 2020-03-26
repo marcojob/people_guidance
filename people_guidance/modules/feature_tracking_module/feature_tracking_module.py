f198d24f4adcc6fe65ee25fbf9a765b684e773fa
Adrian Schneebeli <adrschne@student.ethz.ch>
2020-03-26 17:11:28 +0100

    adding sceleton for pose requests

--- people_guidance/modules/feature_tracking_module/feature_tracking_module.py
+++ people_guidance/modules/feature_tracking_module/feature_tracking_module.py
@@ -13,14 +13,16 @@ class FeatureTrackingModule(Module):
 
     def __init__(self, log_dir: pathlib.Path, args=None):
         super(FeatureTrackingModule, self).__init__(name="feature_tracking_module", outputs=[("feature_point_pairs", 10)],
-                                                    inputs=["drivers_module:images"], log_dir=log_dir)
+                                                    inputs=["drivers_module:images"], #requests=[("position_estimation_module:pose")]
+                                                    log_dir=log_dir)
 
     def start(self):
-        self.logger.info("Starting feature tracking module...")
-
         self.old_img_timestamp = None
         self.old_img_keypoints = None
         self.old_img_descriptors = None
+        self.old_img_pose = None
+
+        self.request_counter = 0
 
         # maximum numbers of keypoints to keep and calculate descriptors of,
         # reducing this number can improve computation time:
@@ -35,12 +37,15 @@ class FeatureTrackingModule(Module):
 
             if not new_img_dict:
                 sleep(1)
-
             else:
                 # extract the image data and time stamp
-                new_img_encoded = new_img_dict['data']
-                new_img_timestamp = new_img_dict['timestamp']
-
+                new_img_encoded = new_img_dict["data"]
+                new_img_timestamp = new_img_dict["timestamp"]
+                """
+                # request the pose of the camera at this time stamp from the position_estimation_module
+                self.make_request("position_estimation_module:pose", {"id" : self.request_counter, "payload": new_img_timestamp})
+                self.request_counter += 1
+                """
                 self.logger.debug(
                     f"Processing image with timestamp {new_img_timestamp} ...")
                 # read the image jpg into an opencv matrix
@@ -53,6 +58,13 @@ class FeatureTrackingModule(Module):
                     new_img, new_img_keypoints)
                 self.logger.debug(
                     f"Found {len(new_img_keypoints)} feautures in this image")
+                """
+                # get the new pose and compute the difference to the old one
+                pose_response = self.await_response("position_estimation_module:pose")
+                new_img_pose = pose_response["payload"]
+                """
+                new_img_pose = 0
+                
 
                 # only do feature matching if there were keypoints found in the new image, discard it otherwise
                 if len(new_img_keypoints) == 0:
@@ -88,8 +100,12 @@ class FeatureTrackingModule(Module):
                                 (old_img_match_points.reshape(-1, 1, 2),
                                  new_img_match_points.reshape(-1, 1, 2)),
                                  axis=1)
+
+                            delta_pose = self.compute_delta_pose(new_img_pose)
                             self.publish("feature_point_pairs",
-                                         matches_paired, 1000, new_img_timestamp)
+                                         {"timestamps": (self.old_img_timestamp, new_img_timestamp), 
+                                          "matches": matches_paired, "delta_pose": delta_pose},
+                                         1000, new_img_timestamp)
 
                     # store the date of the new image as old_img... for the next iteration
                     # If there are no features found in the new image this step is skipped
@@ -97,3 +113,8 @@ class FeatureTrackingModule(Module):
                     self.old_img_timestamp = new_img_timestamp
                     self.old_img_keypoints = new_img_keypoints
                     self.old_img_descriptors = new_img_descriptors
+                    self.old_img_pose = new_img_pose
+
+    def compute_delta_pose(self, new_pose):
+        # Do some fancy calculations here but in the end it's just
+        return new_pose - self.old_img_pose # anyway
