508044bba417487d6d5b398a6f6ef0a5e5fa8bfd
Adrian Schneebeli <adrschne@student.ethz.ch>
2020-03-26 14:53:27 +0100

    adding feature tracking module without requests for pose yet

---
+++ people_guidance/modules/feature_tracking_module/__init__.py
@@ -0,0 +1,1 @@
+from .feature_tracking_module import *
---
+++ people_guidance/modules/feature_tracking_module/feature_tracking_module.py
@@ -0,0 +1,99 @@
+import pathlib
+
+from time import sleep
+
+from people_guidance.modules.module import Module
+from people_guidance.utils import project_path
+
+import cv2
+import numpy as np
+
+
+class FeatureTrackingModule(Module):
+
+    def __init__(self, log_dir: pathlib.Path, args=None):
+        super(FeatureTrackingModule, self).__init__(name="feature_tracking_module", outputs=[("feature_point_pairs", 10)],
+                                                    inputs=["drivers_module:images"], log_dir=log_dir)
+
+    def start(self):
+        self.logger.info("Starting feature tracking module...")
+
+        self.old_img_timestamp = None
+        self.old_img_keypoints = None
+        self.old_img_descriptors = None
+
+        # maximum numbers of keypoints to keep and calculate descriptors of,
+        # reducing this number can improve computation time:
+        self.max_num_keypoints = 100
+
+        # create cv2 ORB feature descriptor and brute force matcher object
+        self.orb = cv2.ORB_create(nfeatures=self.max_num_keypoints)
+        self.matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING, crossCheck=True)
+
+        while True:
+            new_img_dict = self.get("drivers_module:images")
+
+            if not new_img_dict:
+                sleep(1)
+
+            else:
+                # extract the image data and time stamp
+                new_img_encoded = new_img_dict['data']
+                new_img_timestamp = new_img_dict['timestamp']
+
+                self.logger.debug(
+                    f"Processing image with timestamp {new_img_timestamp} ...")
+                # read the image jpg into an opencv matrix
+                new_img = cv2.imdecode(np.frombuffer(
+                    new_img_encoded, dtype=np.int8), flags=cv2.IMREAD_COLOR)
+
+                # first detect the ORB keypoints and then compute the feature descriptors of those points
+                new_img_keypoints = self.orb.detect(new_img, None)
+                new_img_keypoints, new_img_descriptors = self.orb.compute(
+                    new_img, new_img_keypoints)
+                self.logger.debug(
+                    f"Found {len(new_img_keypoints)} feautures in this image")
+
+                # only do feature matching if there were keypoints found in the new image, discard it otherwise
+                if len(new_img_keypoints) == 0:
+                    self.logger.warn(
+                        f"Didn't find any features in image with timestamp {new_img_timestamp}, skipping...")
+                else:
+                    if self.old_img_descriptors is not None:  # skip the matching step for the first image
+                        # match the feature descriptors of the old and new image
+                        matches = self.matcher.match(new_img_descriptors,
+                                                     self.old_img_descriptors)
+
+                        if len(matches) == 0:
+                            # there were 0 matches found, print a warning
+                            self.logger.warn("Couldn't find any matching features in the images with timestamps: " +
+                                             f"{old_img_timestamp} and {new_img_timestamp}")
+
+                        else:
+                            # sort the matches by shortest distance first
+                            matches_sorted = sorted(
+                                matches, key=lambda x: x.distance)
+
+                            # assemble the coordinates of the matched features into a numpy matrix for each image
+                            new_img_match_points = np.float32(
+                                [new_img_keypoints[match.queryIdx].pt for match in matches_sorted])
+                            old_img_match_points = np.float32(
+                                [self.old_img_keypoints[match.trainIdx].pt for match in matches_sorted])
+
+                            # add the two matrixes together and publish the data, first dimension are all the matches,
+                            # second dimension is image 1 and 2, thrid dimension is x and y
+                            # e.g. 4th match, 1st image, y-coordinate: matches_paired[3][0][1]
+                            #      8th match, 2nd image, x-coordinate: matches_paired[7][1][0]
+                            matches_paired = np.concatenate(
+                                (old_img_match_points.reshape(-1, 1, 2),
+                                 new_img_match_points.reshape(-1, 1, 2)),
+                                 axis=1)
+                            self.publish("feature_point_pairs",
+                                         matches_paired, 1000, new_img_timestamp)
+
+                    # store the date of the new image as old_img... for the next iteration
+                    # If there are no features found in the new image this step is skipped
+                    # This means that the next image will be compared witht he same old image again
+                    self.old_img_timestamp = new_img_timestamp
+                    self.old_img_keypoints = new_img_keypoints
+                    self.old_img_descriptors = new_img_descriptors
