8fc285eb89a7f717b8991c4d3e3e9036e629a428
marcojob <marco.job@bluewin.ch>
2020-03-29 16:56:06 +0200

    change frequency

--- people_guidance/modules/visualization_module/visualization_module.py
+++ people_guidance/modules/visualization_module/visualization_module.py
@@ -11,7 +11,7 @@ from ..module import Module
 HOST = "127.0.0.1"  # Host IP
 PORT = 65432  # Port
 PREVIEW_FRAMESIZE = (640, 480)
-POS_PLOT_HZ = 1  # Discard before plotting
+POS_PLOT_HZ = 20  # Discard before plotting
 PREVIEW_PLOT_HZ = 20  # Discard before plotting
 
 
