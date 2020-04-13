4611267c2ff262513fb2d59de8b10316afcbfa49
lorenz-h <lorenz.hetzel@me.com>
2020-04-13 11:12:54 +0200

    add proper file and socket closing, improve pep8 conformity

--- people_guidance/modules/visualization_module/visualization_module.py
+++ people_guidance/modules/visualization_module/visualization_module.py
@@ -30,194 +30,196 @@ class VisualizationModule(Module):
 
     def start(self):
         self.logger.info("Starting visualization module...")
+        with self:
 
+            pos_last_ms = None
+            vis_pos_last_ms = self.get_time_ms()
+
+            repoints_last_ms = None
+            vis_repoints_last_ms = self.get_time_ms()
+
+            preview_last_ms = None
+            vis_preview_last_ms = self.get_time_ms()
+
+            features_dict = dict()
+
+            while True:
+                #sleep(1.0/PREVIEW_PLOT_HZ)
+                sleep(0.1)
+                # POS DATA HANDLING
+                if pos_last_ms is None:
+                    pos_vis = self.get("position_estimation_module:position_vis")
+
+                    pos_last_ms = pos_vis.get("timestamp", None)
+                    vis_pos_last_ms = self.get_time_ms()
+                else:
+                    pos_vis = self.get("position_estimation_module:position_vis")
+                    if pos_vis and self.get_time_ms() - vis_pos_last_ms > 1000/POS_PLOT_HZ and pos_vis["timestamp"] - pos_last_ms > 1000/POS_PLOT_HZ:
+                        pos_last_ms = pos_vis["timestamp"]
+                        vis_pos_last_ms = self.get_time_ms()
+
+                        # Encode position data
+                        pos_buf = np.array([pos_vis["data"]["pos_x"],
+                                            pos_vis["data"]["pos_y"],
+                                            pos_vis["data"]["pos_z"],
+                                            pos_vis["data"]["angle_x"],
+                                            pos_vis["data"]["angle_y"],
+                                            pos_vis["data"]["angle_z"]], dtype='float32').tobytes()
+                        # Len of pos_data
+                        buf_len = np.array([len(pos_buf)], dtype='uint32').tobytes()
+
+                        # Encode id to uint8
+                        buf_id = np.array([1], dtype='uint8').tobytes()
+
+                        if not self.args.save_visualization:
+                            # Send ID first
+                            self.sock.sendall(buf_id)
+                            # Send the image length beforehand
+                            self.sock.sendall(buf_len)
+                            # Send data
+                            self.sock.sendall(pos_buf)
+                        else:
+                            self.pos_data.write(f"{pos_vis['timestamp']}: " +
+                                                f"pos_x: {pos_vis['data']['pos_x']}, " +
+                                                f"pos_y: {pos_vis['data']['pos_y']}, " +
+                                                f"pos_z: {pos_vis['data']['pos_z']}, " +
+                                                f"angle_x: {pos_vis['data']['angle_x']}, " +
+                                                f"angle_y: {pos_vis['data']['angle_y']}, " +
+                                                f"angle_z: {pos_vis['data']['angle_z']}\n")
+                            self.pos_data.flush()
+
+                # REPOINTS HANDLING
+                if repoints_last_ms is None:
+                    repoints = self.get("reprojection_module:points3d")
+
+                    repoints_last_ms = repoints.get("timestamp", None)
+                    vis_repoints_last_ms = self.get_time_ms()
+                else:
+                    repoints = self.get("reprojection_module:points3d")
+                    if repoints and self.get_time_ms() - vis_repoints_last_ms > 1000/REPOINTS_PLOT_HZ and repoints["timestamp"] - repoints_last_ms > 1000/REPOINTS_PLOT_HZ:
+                        repoints_last_ms = repoints["timestamp"]
+                        vis_repoints_last_ms = self.get_time_ms()
+
+                        # Encode reprojected points
+                        print(repoints["data"].astype(dtype='float32').shape)
+                        repoints_buf = repoints["data"].astype(dtype='float32').tobytes()
+                        # Len of pos_data
+                        buf_len = np.array([len(repoints_buf)], dtype='uint32').tobytes()
+
+                        # Encode id to uint8
+                        buf_id = np.array([2], dtype='uint8').tobytes()
+
+                        if not self.args.save_visualization:
+                            # Send ID first
+                            self.sock.sendall(buf_id)
+                            # Send the image length beforehand
+                            self.sock.sendall(buf_len)
+                            # Send data
+                            self.sock.sendall(repoints_buf)
+                        else:
+                            timestamp = repoints["timestamp"]
+                            for point in repoints["data"]:
+                                self.repoints_data.write(f"{timestamp}: {point[0]}, {point[1]}, {point[2]}")
+
+                            self.repoints_data.flush()
+
+
+                # PREVIEW IMAGE HANDLING
+                if preview_last_ms is None:
+                    preview = self.get("drivers_module:preview")
+
+                    preview_last_ms = preview.get("timestamp", None)
+                    vis_preview_last_ms = self.get_time_ms()
+                else:
+                    preview = self.get("drivers_module:preview")
+                    if preview and self.get_time_ms() - vis_preview_last_ms > 1000/PREVIEW_PLOT_HZ \
+                            and preview["timestamp"] - preview_last_ms > 1000/PREVIEW_PLOT_HZ:
+                        preview_last_ms = preview["timestamp"]
+                        vis_preview_last_ms = self.get_time_ms()
+
+                        # Decode img to bytes
+                        img_dec = cv2.imdecode(np.frombuffer(
+                            preview["data"], dtype=np.int8), flags=cv2.IMREAD_COLOR)
+
+                        # Draw matches onto image
+                        matches = features_dict.get(preview["timestamp"], None)
+                        img_dec = self.draw_matches(img_dec, matches)
+
+                        # Resize image
+                        img_rs = self.resize_image(img_dec)
+                        # Encode len in uint32
+                        buf_len = np.array([len(img_rs)], dtype='uint32')
+                        buf_len_b = buf_len.tobytes()
+                        # Encode id to uint8
+                        buf_id = np.array([0], dtype='uint8')
+                        buf_id_b = buf_id.tobytes()
+
+                        if not self.args.save_visualization:
+                            # Send ID first
+                            self.sock.sendall(buf_id_b)
+                            # Send the image length beforehand
+                            self.sock.sendall(buf_len_b)
+                            # Send data
+                            self.sock.sendall(img_rs)
+                        else:
+                            self.preview_counter += 1
+                            # Write image
+                            img_name = self.files_dir / 'vis' / f"img_{self.preview_counter:04d}.jpg"
+                            img_f = io.open(img_name, 'wb')
+                            img_f.write(img_rs)
+                            img_f.close()
+
+                            timestamp = preview["timestamp"]
+                            self.preview_data.write(f"{self.preview_counter}: {timestamp}\n")
+                            self.preview_data.flush()
+
+                    features = self.get("feature_tracking_module:feature_point_pairs_vis")
+                    if features:
+                        features_dict[features["timestamp"]] = features["data"]["point_pairs"]
+
+    def __enter__(self):
         if not self.args.save_visualization:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST, PORT))
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((HOST, PORT))
 
         else:
             self.files_dir = Path(self.args.save_visualization)
             (self.files_dir / 'vis').mkdir(parents=True, exist_ok=True)
             self.preview_counter = 0
-
             self.preview_data = (self.files_dir / 'vis_data.txt').open(mode='w')
-
             self.pos_data = (self.files_dir / 'pos_data.txt').open(mode='w')
-
             self.repoints_data = (self.files_dir / 'repoints_data.txt').open(mode='w')
 
-        pos_last_ms = None
-        vis_pos_last_ms = self.get_time_ms()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.sock.shutdown(socket.SHUT_WR)
+        self.sock.close()
 
-        repoints_last_ms = None
-        vis_repoints_last_ms = self.get_time_ms()
+        self.preview_data.close()
+        self.pos_data.close()
+        self.repoints_data.close()
 
-        preview_last_ms = None
-        vis_preview_last_ms = self.get_time_ms()
-
-        features_dict = dict()
-
-        while True:
-            #sleep(1.0/PREVIEW_PLOT_HZ)
-            sleep(0.1)
-            # POS DATA HANDLING
-            if pos_last_ms is None:
-                pos_vis = self.get("position_estimation_module:position_vis")
-
-                pos_last_ms = pos_vis.get("timestamp", None)
-                vis_pos_last_ms = self.get_time_ms()
-            else:
-                pos_vis = self.get("position_estimation_module:position_vis")
-                if pos_vis and self.get_time_ms() - vis_pos_last_ms > 1000/POS_PLOT_HZ and pos_vis["timestamp"] - pos_last_ms > 1000/POS_PLOT_HZ:
-                    pos_last_ms = pos_vis["timestamp"]
-                    vis_pos_last_ms = self.get_time_ms()
-
-                    # Encode position data
-                    pos_buf = np.array([pos_vis["data"]["pos_x"],
-                                        pos_vis["data"]["pos_y"],
-                                        pos_vis["data"]["pos_z"],
-                                        pos_vis["data"]["angle_x"],
-                                        pos_vis["data"]["angle_y"],
-                                        pos_vis["data"]["angle_z"]], dtype='float32').tobytes()
-                    # Len of pos_data
-                    buf_len = np.array([len(pos_buf)], dtype='uint32')
-
-                    # Encode id to uint8
-                    buf_id = np.array([1], dtype='uint8')
-
-                    if not self.args.save_visualization:
-                        # Send ID first
-                        s.sendall(buf_id)
-                        # Send the image length beforehand
-                        s.sendall(buf_len)
-                        # Send data
-                        s.sendall(pos_buf)
-                    else:
-                        self.pos_data.write(f"{pos_vis['timestamp']}: " +
-                                            f"pos_x: {pos_vis['data']['pos_x']}, " +
-                                            f"pos_y: {pos_vis['data']['pos_y']}, " +
-                                            f"pos_z: {pos_vis['data']['pos_z']}, " +
-                                            f"angle_x: {pos_vis['data']['angle_x']}, " +
-                                            f"angle_y: {pos_vis['data']['angle_y']}, " +
-                                            f"angle_z: {pos_vis['data']['angle_z']}\n")
-                        self.pos_data.flush()
-
-            # REPOINTS HANDLING
-            if repoints_last_ms is None:
-                repoints = self.get("reprojection_module:points3d")
-
-                repoints_last_ms = repoints.get("timestamp", None)
-                vis_repoints_last_ms = self.get_time_ms()
-            else:
-                repoints = self.get("reprojection_module:points3d")
-                if repoints and self.get_time_ms() - vis_repoints_last_ms > 1000/REPOINTS_PLOT_HZ and repoints["timestamp"] - repoints_last_ms > 1000/REPOINTS_PLOT_HZ:
-                    repoints_last_ms = repoints["timestamp"]
-                    vis_repoints_last_ms = self.get_time_ms()
-
-                    # Encode reprojected points
-                    print(repoints["data"].astype(dtype='float32').shape)
-                    repoints_buf = repoints["data"].astype(dtype='float32').tobytes()
-                    # Len of pos_data
-                    buf_len = np.array([len(repoints_buf)], dtype='uint32')
-
-                    # Encode id to uint8
-                    buf_id = np.array([2], dtype='uint8')
-
-                    if not self.args.save_visualization:
-                        # Send ID first
-                        s.sendall(buf_id)
-                        # Send the image length beforehand
-                        s.sendall(buf_len)
-                        # Send data
-                        s.sendall(repoints_buf)
-                    else:
-                        timestamp = repoints["timestamp"]
-                        for point in repoints["data"]:
-                            self.repoints_data.write(f"{timestamp}: {point[0]}, {point[1]}, {point[2]}")
-
-                        self.repoints_data.flush()
-
-
-            # PREVIEW IMAGE HANDLING
-            if preview_last_ms is None:
-                preview = self.get("drivers_module:preview")
-
-                preview_last_ms = preview.get("timestamp", None)
-                vis_preview_last_ms = self.get_time_ms()
-            else:
-                preview = self.get("drivers_module:preview")
-                if preview and self.get_time_ms() - vis_preview_last_ms > 1000/PREVIEW_PLOT_HZ \
-                        and preview["timestamp"] - preview_last_ms > 1000/PREVIEW_PLOT_HZ:
-                    preview_last_ms = preview["timestamp"]
-                    vis_preview_last_ms = self.get_time_ms()
-
-                    # Decode img to bytes
-                    img_dec = cv2.imdecode(np.frombuffer(
-                        preview["data"], dtype=np.int8), flags=cv2.IMREAD_COLOR)
-
-                    # Draw matches onto image
-                    matches = features_dict.get(preview["timestamp"], None)
-                    img_dec = self.draw_matches(img_dec, matches)
-
-                    # Resize image
-                    img_rs = self.resize_image(img_dec)
-                    # Encode len in uint32
-                    buf_len = np.array([len(img_rs)], dtype='uint32')
-                    buf_len_b = buf_len.tobytes()
-                    # Encode id to uint8
-                    buf_id = np.array([0], dtype='uint8')
-                    buf_id_b = buf_id.tobytes()
-
-                    if not self.args.save_visualization:
-                        # Send ID first
-                        s.sendall(buf_id_b)
-                        # Send the image length beforehand
-                        s.sendall(buf_len_b)
-                        # Send data
-                        s.sendall(img_rs)
-                    else:
-                        self.preview_counter += 1
-                        # Write image
-                        img_name = self.files_dir / 'vis' / f"img_{self.preview_counter:04d}.jpg"
-                        img_f = io.open(img_name, 'wb')
-                        img_f.write(img_rs)
-                        img_f.close()
-
-                        timestamp = preview["timestamp"]
-                        self.preview_data.write(f"{self.preview_counter}: {timestamp}\n")
-                        self.preview_data.flush()
-
-                features = self.get("feature_tracking_module:feature_point_pairs_vis")
-                if features:
-                    features_dict[features["timestamp"]
-                                ] = features["data"]["point_pairs"]
-
-        # Send EOF to detect end of file
-        s.shutdown(socket.SHUT_WR)
-        s.close()
-
-    def resize_image(self, img):
+    @staticmethod
+    def resize_image(img):
         # Resize img
-        rs = cv2.resize(img, PREVIEW_FRAMESIZE,
-                        interpolation=cv2.INTER_LINEAR)
-
+        rs = cv2.resize(img, PREVIEW_FRAMESIZE, interpolation=cv2.INTER_LINEAR)
         # Encode img to jpeg
         img_enc = cv2.imencode('.jpeg', rs)[1].tobytes()
 
         return img_enc
 
-    def draw_matches(self, img, matches):
-        RADIUS = 30
-        THICKNESS = 3
+    @staticmethod
+    def draw_matches(img, matches):
+        radius = 30
+        thickness = 3
         if matches is not None:
             shape = matches.shape
             for m in range(shape[2]):
                 start_point = (matches[0][0][m], matches[0][1][m])
                 end_point = (matches[1][0][m], matches[1][1][m])
-                img = cv2.circle(img, start_point, RADIUS,
-                                 (255, 0, 0), THICKNESS)
-                img = cv2.circle(img, end_point, RADIUS,
-                                 (0, 0, 255), THICKNESS)
+                img = cv2.circle(img, start_point, radius,
+                                 (255, 0, 0), thickness)
+                img = cv2.circle(img, end_point, radius,
+                                 (0, 0, 255), thickness)
                 img = cv2.arrowedLine(
-                    img, start_point, end_point, (0, 255, 0), THICKNESS)
+                    img, start_point, end_point, (0, 255, 0), thickness)
         return img
