c356646c99a081ecd04b44d4d89b8604fe127b7c
lorenz-h <lorenz.hetzel@me.com>
2020-04-27 16:02:32 +0200

    fix theos hardcoded dataset location

--- people_guidance/utils.py
+++ people_guidance/utils.py
@@ -7,7 +7,7 @@ import coloredlogs
 ROOT_DIR = pathlib.Path(__file__).parent.parent
 ROOT_LOG_DIR = ROOT_DIR / "logs"
 ROOT_DATA_DIR = ROOT_DIR / "data"
-DEFAULT_DATASET = pathlib.Path("C:\\Users\\theop\\Desktop\\3D_vision_datasets\\indoor_dataset_4")
+DEFAULT_DATASET = ROOT_DATA_DIR / "indoor_dataset_4"
 
 
 def project_path(relative_path: str) -> pathlib.Path:
