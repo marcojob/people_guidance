74755f8bb4055275165a1f72dfabf603035e4c2a
marcojob <marco.job@bluewin.ch>
2020-05-09 18:46:17 +0200

    fixed startup

--- tools/record_start_util.py
+++ tools/record_start_util.py
@@ -2,26 +2,41 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import os
 import re
 
-SSD_FOLDER = "/media/pi/SSD/"
+from time import sleep
 
-def button_callback():
+SSD_FOLDER = "/media/pi/SSD/"
+RECORD_TIME = 30
+
+def button_callback(channel):
     folder_number = 0
 
     for name in os.listdir(SSD_FOLDER):
-        out = re.search(r'outdoor_dataset_([0-9]*)')
+        out = re.search(r'outdoor_dataset_([0-9]*)', name)
         if out:
             number = int(out.group(1))
             if folder_number < number:
                 folder_number = number
 
+    if folder_number == 0:
+        folder_number = 1
+
+    folder_number += 1
+
     folder_name = SSD_FOLDER + f"outdoor_dataset_{folder_number:02d}"
-    os.mkdir(folder_name)
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
 
     print(folder_name)
+    os.system(f'timeout {RECORD_TIME} python3 /home/pi/people_guidance/main.py --record {folder_name}')
 
 if __name__ == '__main__':
     GPIO.setwarnings(False) # Ignore warning for now
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+    # GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+    while True:
+        sleep(0.01)
+        if GPIO.input(10) == GPIO.HIGH:
+            button_callback(0)
+
