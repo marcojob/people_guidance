72cc9610bd8170b09655b2e13a3934be4781a88f
marcojob <marco.job@bluewin.ch>
2020-05-11 11:31:28 +0200

    resize and undistort images

--- people_guidance/modules/visualization_module/visualization_module.py
+++ people_guidance/modules/visualization_module/visualization_module.py
@@ -113,11 +113,8 @@ class VisualizationModule(Module):
                 matches = features["data"]["point_pairs"]
                 preview = features["data"]["img"]
 
-                img_dec = cv2.imdecode(np.frombuffer(
-                        preview, dtype=np.int8), flags=cv2.IMREAD_COLOR)
-
                 # Draw matches onto image
-                self.data_dict["preview"] = self.draw_matches(img_dec, matches)
+                self.data_dict["preview"] = self.draw_matches(preview, matches)
 
                 try:
                     self.animate_preview()
