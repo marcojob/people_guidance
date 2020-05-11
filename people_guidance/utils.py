f19a79d9a548ec5003537c785a2f3f3522eb54c0
marcojob <marco.job@bluewin.ch>
2020-05-11 09:57:56 +0200

    fix master

--- people_guidance/utils.py
+++ people_guidance/utils.py
@@ -11,6 +11,8 @@ ROOT_DATA_DIR = ROOT_DIR / "data"
 DEFAULT_DATASET = ROOT_DATA_DIR / "indoor_dataset_6"
 
 INTRINSIC_MATRIX = np.array([[1.29168322e+03, 0.0, 8.10433936e+02], [0.0, 1.29299333e+03, 6.15008893e+02], [0.0, 0.0, 1.0]])
+DISTORTION_COEFFS = np.array([[ 0.1952957 , -0.48124548, -0.00223218, -0.00106617,  0.2668875]])
+
 
 def project_path(relative_path: str) -> pathlib.Path:
     return ROOT_DIR / relative_path
