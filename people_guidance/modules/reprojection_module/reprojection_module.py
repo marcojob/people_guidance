fix rotation matrix from essential matrix

people_guidance/modules/reprojection_module/reprojection_module.py
@@ -20,14 +20,10 @@ class ReprojectionModule(Module):
         self.average_filter = MovingAverageFilter()
         self.use_alignment = False
 
-        self.last_update_ts: Optional[float] = None
-        self.origin_pm: np.array = np.matmul(self.intrinsic_matrix, np.eye(3, 4))
-        self.origin: np.array = np.zeros(3)
-        self.forward_direction: np.array = np.array((0., 0., 1.))
-
-        # self.fig, self.ax = plt.subplots(3, 3, sharex='col', sharey='row')
-        # self.fig.set_figheight(15)
-        # self.fig.set_figwidth(15)
+        self.last_update_ts = None
+        self.origin_pm = np.matmul(self.intrinsic_matrix, np.eye(3, 4))
+        self.origin = np.zeros(3)
+        self.forward_direction = np.array((0., 0., 1.))
 
     def start(self):
 
@@ -42,6 +38,7 @@ class ReprojectionModule(Module):
                 image = homog_payload["data"]["image"]
 
                 offset_pm = np.matmul(self.intrinsic_matrix, homography)
+                #print(offset_pm)
 
                 points_homo = cv2.triangulatePoints(self.origin_pm, offset_pm, point_pairs[0, ...], point_pairs[1, ...])
                 points3d = cv2.convertPointsFromHomogeneous(points_homo.T)
@@ -53,9 +50,9 @@ class ReprojectionModule(Module):
                     point = point[0]
 
                     # Change coordinate system
-                    point[0] =  point_temp[0]
-                    point[1] =  point_temp[1]
-                    point[2] =  point_temp[2]
+                    point[0] =  point_temp[2]
+                    point[1] = -point_temp[0]
+                    point[2] = -point_temp[1]
 
                     # We only expect points in positive x direction
                     if point[0] < 0.0:
@@ -68,6 +65,9 @@ class ReprojectionModule(Module):
                 self.publish("points3d", data=points3d, validity=100, timestamp=self.get_time_ms())
 
                 points2d = self.project3dto2d(homography, points3d)
+
+                #print("repro")
+                #print(homography)
 
                 """
                 if cv2.waitKey(0) == ord('a'):